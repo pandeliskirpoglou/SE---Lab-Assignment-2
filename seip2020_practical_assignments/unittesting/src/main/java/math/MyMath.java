aa93da089b240151422680f3397e489034340ff1
Pandelis Kirpoglou <pand181567@yahoo.com>
2020-04-17 23:17:34 +0300

    Adding isPrime method

--- seip2020_practical_assignments/unittesting/src/main/java/math/MyMath.java
+++ seip2020_practical_assignments/unittesting/src/main/java/math/MyMath.java
@@ -1,7 +1,8 @@
 package math;
 
 /**
- * The MyMath provides simple methods such as computing a factorial
+ * The MyMath provides simple methods such as computing a factorial or finding
+ * whether an integer is prime
  *
  * @author pandeliskirpoglou
  * @version 1.0
@@ -29,6 +30,30 @@ public class MyMath {
 		}
 
 		return fact;
+	}
+
+	/**
+	 * Gets one integer and returns true if it is prime and false if it is not.
+	 * 
+	 * @param n the number we are trying to find out whether it is prime or not
+	 * @return isPrimeNumber true if n is prime | false if n is not prime
+	 * @exception IllegalArgumentException when inputs n < 2
+	 */
+
+	public boolean isPrime(int n) {
+		boolean isPrimeNumber = true;
+		if (n < 2) {
+			throw new IllegalArgumentException("No prime numbers below 2");
+		} else {
+			for (int i = 2; i <= n / 2; ++i) {
+				if (n % i == 0) {
+					isPrimeNumber = false;
+					break;
+				}
+			}
+		}
+
+		return isPrimeNumber;
 	}
 
 }
