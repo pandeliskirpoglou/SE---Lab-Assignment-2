eaadeca8ea261f6bb50199dc8eb1b185a94a5dfd
Pandelis Kirpoglou <pand181567@yahoo.com>
2020-04-17 14:27:45 +0300

    Adding first test cases

--- seip2020_practical_assignments/unittesting/src/test/java/io/FileIOTest.java
+++ seip2020_practical_assignments/unittesting/src/test/java/io/FileIOTest.java
@@ -1,20 +1,60 @@
 package io;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 import io.FileIO;
+
+/**
+ * A class that provides test cases for the FileIO class.
+ * 
+ * @author pandeliskirpoglou (pand181567@yahoo.com)
+ */
 
 public class FileIOTest {
 
 	FileIO fileio = new FileIO();
 	public static String path = "src/test/resources/";
 
+	/*
+	 * A unit test that tests whether a file of numbers is being read correctly
+	 */
+
 	@Test
 	public void testReadFile() {
-		String[] expectedNumbers = new String[] { "3", "9", "0", "2", "10", "9", "3", "8", "0", "3" };
-		String validPath = path.concat("grades.txt");
-		
+		int[] expectedNumbers = new int[] { 3, 9, 0, 2, 10, 9, 3, 8, 0, 3 };
+		String validPath = path.concat("grades_valid.txt");
+
 		Assert.assertArrayEquals(expectedNumbers, fileio.readFile(validPath));
 	}
+
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
+	/*
+	 * A unit test that tests illegal argument exception for invalid path
+	 */
+
+	@Test
+	public void testReadFileNotFound() {
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("Input file does not exist");
+		fileio.readFile("invalidPath");
+	}
+	
+	/*
+	 * A unit test that tests illegal argument exception for invalid path
+	 */
+
+	@Test
+	public void testReadFileEmptyFileInput() {
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("Given file is empty");
+		String emptyFilePath = path.concat("empty_file.txt");
+		fileio.readFile(emptyFilePath);
+	}
+
 
 }
