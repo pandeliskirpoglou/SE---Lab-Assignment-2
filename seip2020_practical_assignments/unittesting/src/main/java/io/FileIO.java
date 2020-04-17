632db190d120d944c4d25020ce9aa37d860ee0c3
Pandelis Kirpoglou <pand181567@yahoo.com>
2020-04-17 14:26:49 +0300

    Updating FileIO with new version

--- seip2020_practical_assignments/unittesting/src/main/java/io/FileIO.java
+++ seip2020_practical_assignments/unittesting/src/main/java/io/FileIO.java
@@ -2,13 +2,14 @@ package io;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
-* Provides simple file input/output operations
+* The FileIO provides simple file input/output operations
 * that serve as hands-on practice on Unit Testing.
 *
 * @author  agkortzis
@@ -18,25 +19,37 @@ import java.util.List;
 public class FileIO {
 	
 	/**
-	 * Reads a file and returns its content in an String[] array 
-	 * @param filepath the file that contains text in lines
-	 * @return an array with the file content
+	 * Reads a file that contains numbers line by line 
+	 * and returns an array of the integers found in the file.
+	 * @param filepath the file that contains the numbers
+	 * @return an array of numbers
+	 * @exception IllegalArgumentException when the given file does not exist
+	 * @exception IllegalArgumentException when the given file is empty
+	 * @exception IOException when an IO interruption occurs (not required to be tested)
 	 */
-	public String[] readFile(String filepath) {
+	public int[] readFile(String filepath) {
 		File file = new File(filepath);
+		if (!file.exists()) 
+			throw new IllegalArgumentException("Input file does not exist");
 		
-		List<String> names = new ArrayList<>();
+		List<Integer> numbersList = new ArrayList<>();
 		BufferedReader reader;
 		try {
 			reader = new BufferedReader(new FileReader(file));
 			String line = null;
 			while ((line = reader.readLine()) != null) {
-				names.add(line);
+				int number = Integer.parseInt(line);
+				numbersList.add(number);
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
-		return names.stream().toArray(String[]::new);
+		if (numbersList.size() == 0) 
+			throw new IllegalArgumentException("Given file is empty");
+		
+		// Convert a List to an array using 
+		return numbersList.stream().mapToInt(i -> i).toArray();
 	}
+
 }
